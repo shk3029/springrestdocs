d1f78ba8108caed1351f19cd6e7dcc6fa7cbf087
JS <shk3029>
2019-10-01 19:00:33 +0900

    Initial commit

---
+++ src/main/java/me/js/springrestdocs/SpringrestdocsApplication.java
@@ -0,0 +1,13 @@
+package me.js.springrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestdocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrestdocsApplication.class, args);
+    }
+
+}
